978ad66883816c534261622aeb478f14dd4d78dc
arpitpardesi <arpit.pardesi6@gmail.com>
2022-10-26 18:10:48 +0530

    React app created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello!!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
