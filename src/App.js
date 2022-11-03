53e9fc7d379508757a64d7f3af005f0bfb061226
arpitpardesi <arpit.pardesi6@gmail.com>
2022-11-04 00:40:09 +0530

    added favicon, Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* Header */}
+      <Header/>
+      <h1>Hello!!</h1>
         <p>
           Arpit Pardesi
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        {/* Footer */}
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/footer.css"
+
+const Footer = () => {
+    return(
+        <div>
+            <div>Footer</div>
+        </div>
+    )
+}
+export default Footer;
