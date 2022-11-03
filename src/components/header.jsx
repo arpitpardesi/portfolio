0392be20422ecc04d67274f6532b72ed504d3adb
arpitpardesi <arpit.pardesi6@gmail.com>
2022-11-04 00:40:09 +0530

    added favicon, Header and Footer

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/header.css";
+
+
+const Header = () => {
+    return(
+        <div className="container">
+            <div>
+                Header
+            </div>
+        </div>
+    )
+}
+export default Header;
